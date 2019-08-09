593ecd2fa9535072630a0e5d7db10faee6ddf6d1
Justine Gennaro <justine.rouzier@gmail.com>
2019-08-09 11:20:17 -0500

    added classes and appended to elements

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,11 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector('.cards-container')
+
 
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
 .then(response => {
     console.log('response: ',response.data.articles)
+
 })
 .catch(error => {
     console.log('This is an error: ', error)
 })
+
+
+function CreateCard(articleHeadline, articleImage, articleAuthor) {
+    // Create elements
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const img = document.createElement('a')
+    const authorName = document.createElement('span')
+
+    // Add classes
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    // Add content
+    // headline.textContent = 
+    // img.src = 
+    // authorName.textContent = 
+
+    // Append
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgContainer)
+    imgContainer.appendChild(img)
+    author.appendChild(authorName)
+
+
+    return card;
+}
+
+CreateCard()
